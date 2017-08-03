ad880f13a77039c018ee9e308b69cf24df3a0e21
luis.martins <luis.martins@vilt-group.com>
2017-08-03 16:21:03 +0100

    FIX: Change build reports script
    * Change event and dashbords name
    * Add collection creation
    
    Refs: #1

--- src/metabase/sync_database.clj
+++ src/metabase/sync_database.clj
@@ -12,6 +12,7 @@
             [metabase.sync-database
              [analyze :as analyze]
              [introspect :as introspect]
+             [build-reports :as build-reports]
              [sync :as sync]
              [sync-dynamic :as sync-dynamic]]
             [toucan.db :as db]))
@@ -44,6 +45,7 @@
           (driver/sync-in-context db-driver database (partial sync-database-with-tracking! db-driver database full-sync?))
           (finally
             ;; always cleanup our tracking when we are through
+            (build-reports/build database-id (int 1))
             (swap! currently-syncing-dbs disj database-id)))))))
 
 (defn sync-table!
