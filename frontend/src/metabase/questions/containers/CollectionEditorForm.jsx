c680bad464a2cf75128706ecb9b43f1b3a6f55dc
luis.martins <luis.martins@vilt-group.com>
2017-09-20 15:41:31 +0100

    FIX:  Change default collection descripton to readonly
    Refs: #31

--- frontend/src/metabase/questions/containers/CollectionEditorForm.jsx
+++ frontend/src/metabase/questions/containers/CollectionEditorForm.jsx
@@ -60,7 +60,6 @@ export class CollectionEditorForm extends Component {
 
     render() {
         const { fields, onClose } = this.props;
-        const isDefault = (fields.description == "Audit default") ? "true" : "false"
         return (
             <Modal
                 inline
@@ -86,7 +85,7 @@ export class CollectionEditorForm extends Component {
                         {...fields.description}
                     >
                         <textarea
-                            readOnly={isDefault}
+                            readOnly={fields.description.value == "Audit default"}
                             className="Form-input full"
                             placeholder="It's optional but oh, so helpful"
                             {...fields.description}
